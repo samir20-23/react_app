5874de359af484b669129218fa8ff1606e6b896e
samir20-23 <tistenow@gmail.com>
2024-11-01 00:15:36 +0100

    coding

--- my_application/src/App.js
+++ my_application/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${clicked ? 'clicked' : ''}`}>
+      <h1 className="header">SAMIR</h1>
+      <h2 className="top-text">Welcome to My React Test Project</h2>
+      <button className="center-button" onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   );
 }
